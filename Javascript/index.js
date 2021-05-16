b255dbd2e695469d012a5b0a6e5ba0d11f4395e9
Tanishq Arnav Beck <tanishqarnav@outlook.com>
2021-05-16 16:27:10 +0530

    .

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,16 @@
-var a
-
-function addFive(a) {
-    a += 5
+function inSwitch () {
+    var num = ''
+    switch(val) {
+        case 1:
+            num = 'alpha';
+            break;
+        case 2: 
+            num = 'beta';
+            break;
+        case 3:
+            num = 'gama';
+            break;
+    }
+    return num;
 }
-
-console.log(addFive(5))
+console.log(inSwitch(2))
